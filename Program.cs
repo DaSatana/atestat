351bca2cfc878b9401cc38ddfa6131f894584c14
Bog <dasatana@ya.ru>
2023-11-22 18:50:19 +0300

    add program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] arr = new string[] { "Hello", "2", "world", ":-)" };
+
+string[] result = FilterArray(arr);
+
+Console.WriteLine("Исходный массив:");
+PrintArray(arr);
+Console.WriteLine("Отфильтрованный массив:");
+PrintArray(result);
+
+
+static string[] FilterArray(string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+
+    string[] result = new string[count];
+    int index = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            result[index] = arr[i];
+            index++;
+        }
+    }
+
+    return result;
+}
+
+static void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine(arr[i]);
+    }
+}
